51f2c61108df0ca2e433a9e1d947d7031d8a1c8d
Zac <zombiezac@att.net>
2018-10-11 13:56:24 -0600

    Initial commit

---
+++ ChangeLabeliOSProject/Controller/LabelChangerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ChangeLabeliOSProject
+//
+//  Created by Durham, Zachary on 10/11/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class LabelChangerViewController: UIViewController
+{
+    private lazy var myLabel : LabelTool = LabelTool()
+    
+    @IBOutlet weak var labelButton: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    var text: String?
+    
+    @IBAction func labelButton(_ sender: UIButton)
+    {
+        label.text = "Memerino"
+        label.text = "Memes"
+    }
+}
+
---
+++ ChangeLabeliOSProject/Model/LabelTool.swift
@@ -0,0 +1,15 @@
+//
+//  LabelTool.swift
+//  ChangeLabeliOSProject
+//
+//  Created by Durham, Zachary on 10/11/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class LabelTool
+{
+
+}
